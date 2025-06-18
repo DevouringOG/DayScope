44d70c203afbc012ee49de1aae213e046e3abeae
DEVOURING <DevouringOG@yandex.ru>
2025-06-18 16:17:04 +0500

    setup default redis

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,8 +1,10 @@
 from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.redis import RedisStorage, DefaultKeyBuilder
 from aiogram_dialog import setup_dialogs
 from sqlalchemy.ext.asyncio import async_sessionmaker
 from fluentogram import TranslatorHub
 import structlog
+from redis.asyncio.client import Redis
 
 from bot.handling.handlers import start_router
 from bot.handling.dialogs import first_start_dialog, create_task_dialog
@@ -15,7 +17,12 @@ async def main(config: Config, session_maker: async_sessionmaker):
     log = structlog.get_logger(__name__)
     log.info("INFO")
     bot = Bot(token=config.token.get_secret_value())
-    dp = Dispatcher()
+    redis = Redis(host="localhost")
+    storage = RedisStorage(
+        redis=redis,
+        key_builder=DefaultKeyBuilder(with_destiny=True),
+    )
+    dp = Dispatcher(storage=storage)
 
     translator_hub: TranslatorHub = i18n_factory()
 
